e221ac178e0119d41349e6f875c3f1d947c3cee7
TjoMas-A <thomasanderson@Macintosh.local>
2014-09-27 16:55:46 +0100

    Mod Class Done!

--- src/main/java/com/tjomas_a/modone/ModOne.java
+++ src/main/java/com/tjomas_a/modone/ModOne.java
@@ -1,7 +1,31 @@
 package com.tjomas_a.modone;
 
-/**
- * Created by thomasanderson on 27/09/2014.
- */
-public class ModOne {
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="ModOne", name = "Mod One", version = "1.7.10-1.0")
+public class ModOne
+{
+    @Mod.Instance("ModOne")
+    public static ModOne instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
 }
