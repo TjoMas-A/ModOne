58f6562fc34a57ccd6c373056b9d60b9048a3dd7
TjoMas-A <thomasanderson@Macintosh.local>
2014-11-20 20:56:45 +0000

    What is this? Something IRL?

---
+++ src/main/java/com/tjomas_a/modone/block/BlockNovaculite.java
@@ -0,0 +1,13 @@
+package com.tjomas_a.modone.block;
+
+public class BlockNovaculite extends BlockMO
+{
+    public BlockNovaculite()
+    {
+        super();
+        this.setBlockName("novaculite");
+        this.setBlockTextureName("novaculite");
+        this.setHardness(2.5F);
+        this.setResistance(75.0F);
+    }
+}
--- src/main/java/com/tjomas_a/modone/init/ModBlocks.java
+++ src/main/java/com/tjomas_a/modone/init/ModBlocks.java
@@ -2,6 +2,7 @@ package com.tjomas_a.modone.init;
 
 import com.tjomas_a.modone.block.BlockHealingStation;
 import com.tjomas_a.modone.block.BlockMO;
+import com.tjomas_a.modone.block.BlockNovaculite;
 import com.tjomas_a.modone.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -9,9 +10,11 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class ModBlocks
 {
     public static final BlockMO healingStation = new BlockHealingStation();
+    public static final BlockMO novaculite = new BlockNovaculite();
 
     public static void init()
     {
         GameRegistry.registerBlock(healingStation, "healingStation");
+        GameRegistry.registerBlock(novaculite, "novaculite");
     }
 }
